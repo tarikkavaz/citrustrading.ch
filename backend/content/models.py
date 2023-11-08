90d424fb2072a169f2e9d4e12b61988913f8f0b6
Tarik Kavaz <tarikkavaz@gmail.com>
2023-11-08 10:27:49 +0300

    linkedin

--- backend/content/models.py
+++ backend/content/models.py
@@ -158,6 +158,7 @@ class Social(models.Model):
     instagram = models.CharField(max_length=255, blank=True, validators=[URLValidator()])
     threads = models.CharField(max_length=255, blank=True, validators=[URLValidator()])
     youtube = models.CharField(max_length=255, blank=True, validators=[URLValidator()])
+    linkedin = models.CharField(max_length=255, blank=True, validators=[URLValidator()])
     order = models.PositiveIntegerField(default=0, db_index=True)
 
     class Meta:
