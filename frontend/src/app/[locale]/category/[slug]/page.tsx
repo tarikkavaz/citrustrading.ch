54b5123ace39fc36274ba6aab4e3e15d88e54c11
Tarik Kavaz <tarikkavaz@gmail.com>
2023-11-15 11:06:21 +0300

    fix pip

--- frontend/src/app/[locale]/category/[slug]/page.tsx
+++ frontend/src/app/[locale]/category/[slug]/page.tsx
@@ -72,7 +72,7 @@ export default async function CategoryPage({
 
   return (
     <>
-      {/* <div className="bg-white">
+      <div className="bg-white">
         <div className="relative isolate overflow-hidden bg-gradient-to-b from-indigo-100/20 pt-14">
           <div
             className="absolute inset-y-0 right-1/2 -z-10 -mr-96 w-[200%] origin-top-right skew-x-[-30deg] bg-white shadow-xl shadow-indigo-600/10 ring-1 ring-indigo-50 sm:-mr-80 lg:-mr-96"
@@ -110,7 +110,7 @@ export default async function CategoryPage({
           </div>
           <div className="absolute inset-x-0 bottom-0 -z-10 h-24 bg-gradient-to-t from-white sm:h-32" />
         </div>
-      </div> */}
+      </div>
       <Container className="p-10 mt-16">
         <h1>{t("category")}: {category?.title || slug}</h1>
         <div>
