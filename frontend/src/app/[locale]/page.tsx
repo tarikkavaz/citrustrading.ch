3412a2ea0566878e221e82691f4fed9d8b8145b9
Tarik Kavaz <tarikkavaz@gmail.com>
2023-11-18 12:09:07 +0300

    if no image

--- frontend/src/app/[locale]/page.tsx
+++ frontend/src/app/[locale]/page.tsx
@@ -89,12 +89,14 @@ export default async function Products({ params: { locale } }: HomeProps) {
             </div>
           </div>
           <div className="lg:absolute lg:inset-y-0 lg:right-0 lg:w-1/2">
-            {homepage.images && homepage.images.length > 0 && (
-              <GlobalCarousel
-                images={homepage.images || []} 
-                className="aspect-[3/2] object-cover lg:aspect-auto lg:h-full lg:w-full rounded-2xl  md:rounded-none md:rounded-bl-[150px]" 
-              />
-            )}
+          {homepage.images && homepage.images.length > 0 ? (
+            <GlobalCarousel
+              images={homepage.images} 
+              className="aspect-[3/2] object-cover lg:aspect-auto lg:h-full lg:w-full rounded-2xl md:rounded-none md:rounded-bl-[150px]" 
+            />
+          ) : (
+            <img src="/images/placeholder.jpg" alt="Placeholder" className="aspect-[3/2] object-cover lg:aspect-auto lg:h-full lg:w-full rounded-2xl md:rounded-none md:rounded-bl-[150px]" />
+          )}
           </div>
         </div>
       </Container>
