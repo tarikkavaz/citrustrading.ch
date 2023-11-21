a78171b6fcb8b8d6cbb445c9b3679c93cee02d75
Tarik Kavaz <tarikkavaz@gmail.com>
2023-11-21 13:06:44 +0300

    pagetransition component

--- frontend/src/components/animation/PageTransition.tsx
+++ frontend/src/components/animation/PageTransition.tsx
@@ -1,27 +1,27 @@
-'use client';
+"use client"
 import { motion, AnimatePresence } from 'framer-motion';
 import React from 'react';
-
-const pageTransitionVariants = {
-  initial: { opacity: 0 },
-  animate: { 
-    opacity: 1, 
-    transition: { duration: 1 }
-  },
-  exit: { 
-    opacity: 0, 
-    transition: { duration: 1 }
-  },
-};
 
 interface PageTransitionProps {
   children: React.ReactNode;
   keyProp: string;
 }
 
+const pageTransitionVariants = {
+  initial: { opacity: 0 }, // Start with zero opacity
+  animate: {
+    opacity: 1, // Fade in to full opacity
+    transition: { duration: 1 }
+  },
+  exit: {
+    opacity: 0, // Fade out to zero opacity
+    transition: { duration: 1 }
+  },
+};
+
 const PageTransition: React.FC<PageTransitionProps> = ({ children, keyProp }) => {
   return (
-    <AnimatePresence mode="wait">
+    <AnimatePresence>
       <motion.main
         key={keyProp}
         variants={pageTransitionVariants}
